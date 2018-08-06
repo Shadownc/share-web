52096eb9a12f27e729b4abd5cded0bdf24061b37
shadow <365172043@qq.com>
2018-08-06 20:21:55 +0800

    post 502

--- routes/sign.js
+++ routes/sign.js
@@ -92,8 +92,8 @@ router.get('/getInfo', (req, res) => {
 });
 //退出登录
 router.get('/logout', (req, res) => {
-    req.session.user = null;
-    req.clearCookie('shadow');
+    //req.session.user = null;
+    req.session.destroy();
     res.status(200).json({ code: '00', message: '退出成功' });
 });
 
